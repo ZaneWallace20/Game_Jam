612959323fc2b858d746d93a6e6cae523fd4028d
ZaneWallace20 <mannonzanewallace@gmail.com>
2024-11-20 00:01:05 -0600

    Better Audio
    Subway audio has a better delay, tv has static, light has pitched audio per brightness.

--- Game/Imports/omni_light_3d.gd
+++ Game/Imports/omni_light_3d.gd
@@ -1,7 +1,17 @@
 extends OmniLight3D
 
 @export var noise: NoiseTexture3D
+
+@onready var electric_sound: AudioStreamPlayer3D = $"../electric_sound"
+
+
 var time_passed := 0.0
+
+var last_noise = 0
+
+var electric_cool_down = 0.1
+var set_electric_cool_down = 0.5
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time_passed += delta
@@ -10,3 +20,12 @@ func _process(delta: float) -> void:
 	sampled_noise = abs(sampled_noise)
 	
 	light_energy = sampled_noise * 10
+	
+	
+	
+	electric_sound.pitch_scale = clamp(sampled_noise * 10, 0.95, 1.0) - .2
+	electric_sound.volume_db = lerp(-40, -15, sampled_noise) 
+	
+
+	
+	
